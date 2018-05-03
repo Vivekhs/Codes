d8b26f1cc97ac1b797d782cd14cf1332d3606cd8
Vivek Sinha <viveks@latitudefintech.com>
2018-05-03 18:22:42 +0530

    changes done

--- src/com/problems/NoOfOnes.java
+++ src/com/problems/NoOfOnes.java
@@ -16,15 +16,15 @@ public class NoOfOnes {
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println(" jnje"+(-1<<30));
-		int n = sc.nextInt();
-		int count = 0;
-		System.out.println("binary "+Integer.toBinaryString((-1<<31)));
-		while(n>0){
-			n = n & (n-1);
-			count++;
-		}
-		System.out.println("Hello Vivek count is "+count+" "+(1 << 31 & -1 >>> 0));
+		System.out.println(" jnje"+Integer.toBinaryString(2>>1));
+//		int n = sc.nextInt();
+//		int count = 0;
+//		System.out.println("binary "+Integer.toBinaryString((-1<<31)));
+//		while(n>0){
+//			n = n & (n-1);
+//			count++;
+//		}
+//		System.out.println("Hello Vivek count is "+count+" "+(1 << 31 & -1 >>> 0));
 		
 		sc.close();
 	}
